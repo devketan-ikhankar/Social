7d3081b901be9f7edbeafc4342b31478f61a37ac
ketan-0702 <ketanikhankar2@gmail.com>
2025-05-15 12:30:03 +0530

    first task

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,42 @@
 import { useState } from 'react'
-import Login from './login'
-import Register from './register'
+
 
 import './App.css'
 
 
 
 function App() {
-  const [send, setSend] = useState('')
-  // const [count, setCount] = useState(0)
-  // const [name, setName] = useState('ketan')
+ 
+  const [count, setCount] = useState(0)
+  const [name, setName] = useState('')
 
-  // const handleClick = () => {
-  //   if (count >= 5) {
-  //     return;
-  //   }
-  //   setCount((count) => count + 1)
-
-
-
-  // }
-
-
-  // const handleChange = (e: { target: { value: SetStateAction<string> } }) => {
-
-  //   setName(e.target.value)
-
-
-  // }
+  const handleClick = () => {
+    if (count >= 5) {
+      return;
+    }
+    setCount((count) => count + 1)
 
 
 
-  // const handleClick1 = (e: {target: { value: SetStateAction<string> } }) => {
-  //   setPassword(e.target.value)
+  }
 
-  // }
+
+  const handleChange = (e: { target: { value: SetStateAction<string> } }) => {
+
+    setName(e.target.value)
+
+
+  }
+
+
+
+  const handleClick1 = (e: {target: { value: SetStateAction<string> } }) => {
+    setPassword(e.target.value)
+
+  }
   return (
     <>
-      {/* <div>
-      
-      </div>
+     
      
       <div className="card">
         <button onClick={handleClick}>
@@ -55,18 +52,11 @@ function App() {
       <input style={{ width: '200px', height: '30px', borderRadius: '5px', border: '1px solid black' }}
         onChange={handleChange}
 
-        type="text" placeholder="Enter your name" /> */}
+        type="text" placeholder="Enter your name" />
 
+ 
 
-
-      <div>
-        <p>{send}</p>
-        <Login send={setSend} />
-        <Register
-
-
-        />
-      </div>
+     
     </>
   )
 }
